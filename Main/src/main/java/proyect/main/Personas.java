575be31bee3ff975bcdb6c593f6fb1cfc020bde5
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2024-04-03 23:17:09 -0600

    prueba

--- Main/src/main/java/proyect/main/Personas.java
+++ Main/src/main/java/proyect/main/Personas.java
@@ -10,5 +10,20 @@ public class Personas {
         this.name = name;
         this.edad = edad;
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
