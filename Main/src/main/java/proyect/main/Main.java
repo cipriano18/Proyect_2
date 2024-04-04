8e812ee731af56b8cdc576feb271f2a55dc85648
Stiff-2904 <albertstiff102030@gmail.com>
2024-04-03 23:19:21 -0600

    prueba
    nosexd

--- Main/src/main/java/proyect/main/Main.java
+++ Main/src/main/java/proyect/main/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    public static void xd(){
+        System.out.println("HOLAAAAAAAAAAAAAAAAAAAAAA");
+    }
 }
